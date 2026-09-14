e2039aab53ada76a403e1088b2f685595b1ab05a
mazeneslam553-create <mazeneslam553@gmail.com>
2026-09-14 19:48:31 +0300

    Add files via upload

---
+++ filterPassed.js
@@ -0,0 +1,12 @@
+const students = require('../data/students');
+const calculateAverage = require('./calculateAverage');
+
+/**
+ * Filters students whose average grade is >= 60 (passing).
+ * @returns {object[]} Array of student records who passed.
+ */
+function filterPassed() {
+  return students.filter((student) => calculateAverage(student.grades) >= 60);
+}
+
+module.exports = filterPassed;
